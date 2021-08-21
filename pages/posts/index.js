cb194194cfc02305562eb8af1b40a72495292d11
vkoschel <victorkoschel@gmail.com>
2021-08-21 10:14:38 -0300

    all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started2",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started3",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started4",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
