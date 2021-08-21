ce31c6eb1f6f284a658c9bcd537f3f4ad510ce51
vkoschel <victorkoschel@gmail.com>
2021-08-21 10:01:07 -0300

    setting up navigation and featured posts components on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import Hero from '../components/home-page/hero';
+import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started2",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started3",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started4",
+    title: "Getting Started",
+    image: "getting-started.png",
+    excerpt: "Here's what you need to do",
+    date: "2022-02-10",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
