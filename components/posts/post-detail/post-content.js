e5c4c26dc13630229b764202f89dd871e17926f5
vkoschel <victorkoschel@gmail.com>
2021-08-22 09:08:17 -0300

    post detail page and rendering markdown content

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,24 @@
+import ReactMarkdown from "react-markdown";
+
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
+const DUMMY_POST = {
+  slug: "getting-started",
+  title: "Getting Started",
+  image: "getting-started.png",
+  date: "2022-02-10",
+  content: "# This is a first test post MD",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
