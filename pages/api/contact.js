f6bdbe52d4a8b53c302fc4d09a0065676a0d6593
vkoschel <victorkoschel@gmail.com>
2021-08-31 20:11:05 -0300

    store messages in mongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,8 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+import { USER, PASS } from "../../config";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = JSON.parse(req.body);
 
@@ -19,7 +23,29 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://${USER}:${PASS}@cluster0.abpur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "connection to db failed" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "failed to save message" });
+      return;
+    }
+
+    client.close();
+
     res
       .status(201)
       .json({ message: "successfully stored message!", message: newMessage });
