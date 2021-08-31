5b550a531e4c646b5bb6ab6044c084aff236bb0c
vkoschel <victorkoschel@gmail.com>
2021-08-31 19:43:38 -0300

    send form data to api route

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 function handler(req, res) {
   if (req.method === "POST") {
-    const { email, name, message } = req.body;
+    const { email, name, message } = JSON.parse(req.body);
 
     if (
       !email ||
